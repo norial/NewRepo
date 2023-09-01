e2b209984a37801687834e95d89fe491bf9a43e2
Olejjas <mollstam4455@gmail.com>
2023-09-02 00:34:33 +0300

    Homework 2 is solved

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,54 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-//1) Вивести число пі
+﻿//Домашнє завдання #2
 
-//1
-//Console.WriteLine(Math.PI);
+//1) Дано тризначне число. Знайти число, отримане під час прочитання його цифр справа наліво.
 
-//2
-//Console.WriteLine(Math.Acos(-1));
-
-//2) поміняти змінні місцями 2
-//byte a = 1;
-//byte b = 5;
-
-
-//1
-//c = b;
-//b = a;
-//a = c;
-
-
-//2
-
-//a = (byte) (a + b);
-//b = (byte) (a - b);
-//a = (byte) (a - b);
-
-//Console.WriteLine(a);
-//Console.WriteLine(b);
-
-
-//3) перевести см у м
-//float dist;
-
-//Console.WriteLine("Enter distance!");
-//string input = Console.ReadLine();
-//dist = float.Parse(input);
-//dist /= 100;
-//Console.WriteLine("distance: {0}", dist);
-
-//4)
-
-
-//Console.Write("Write value 10-99: ");
+//Console.Write("enter a number between 100 and 999: ");
 //int inputNum = int.Parse(Console.ReadLine());
-//int firstDigit = inputNum / 10;
-//int secondDigit = inputNum % 10;
+//int ones = inputNum % 10;
+//int tens = (inputNum % 100) / 10; ;
+//int hundreds = inputNum / 100;
+//int ReverseInput = ones*100 + tens*10 + hundreds;
+//Console.WriteLine("{0}{1}{2}", ones, tens, hundreds);
+//Console.WriteLine($"Reverse number is :{ReverseInput}");
 
 
-//Console.WriteLine($"{firstDigit}") ;
-//Console.WriteLine($"{secondDigit}") ;
-//Console.WriteLine($"{firstDigit + secondDigit}") ;
-//Console.WriteLine($"{firstDigit * secondDigit}") ;
+
+//2) Написати програму, яка обчислює квадрат будь-якого введеного числа.
+
+//Console.Write("Enter the number whose square root you want to find: ");
+//int inputNum = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Your square root is: {inputNum *= inputNum}");
+
+
+
+//3) Дано тризначне число. Знайти число, отримане під час перестановки першої та другої цифр заданого числа.
+
+//Console.Write("enter a number between 100 and 999: ");
+//int inputNum = int.Parse(Console.ReadLine());
+//int ones = inputNum % 10;
+//int tens = (inputNum % 100) / 10; ;
+//int hundreds = inputNum / 100;
+//int ReverseTnOInput = ones + tens * 100 + hundreds*10;
+//Console.WriteLine($"Reverse number is :{ReverseTnOInput}");
+
+
+
+//4) Написати програму, яка обчислює середнє арифметичне двох чисел.
+
+//Console.Write("enter first number: ");
+//float inputNum1 = float.Parse(Console.ReadLine());
+//Console.Write("enter second number: ");
+//float inputNum2 = float.Parse(Console.ReadLine());
+//float ArithmMean = (inputNum1 + inputNum2) / 2;
+//Console.Write($"arithmetic mean of two numbers is equal to: {ArithmMean}");
+
+
+
+//5*) Дослідити інші способи поміняти місцями значення двох змінних без використання тимчасової змінної. 
+
+//1
+int a = 3;
+int b = 5;
+
+//a = a ^ b;
+//b = a ^ b;
+//a = a ^ b;
+
+//Console.WriteLine($"{a}, {b}");
+
+//2
+
+//a = a * b;
+//b = a / b;
+//a = a / b;
+
+//Console.WriteLine($"{a}, {b}");
+
 
 //Console.ReadKey();
