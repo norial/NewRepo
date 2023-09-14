d860c676d842b299eafb0c66b77d6976b172036f
Olejjas <mollstam4455@gmail.com>
2023-09-14 21:03:23 +0300

    HomeWork5 is Done!!!

---
+++ HillelCourse/HomeWork5/InOrderOrNot/Program.cs
@@ -0,0 +1,66 @@
+﻿//Вказано п'ять довільних цілих чисел (елементи масиву).
+//Визначити, чи є їхнє розташування в масиві впорядкованим
+//(тобто за зростанням або за спаданням) чи невпорядкованим.
+
+namespace InOrderOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[5] { 48, 55, 69, 70, 95 };
+            //ArrayFiller(array1);
+            bool isSortedAscending = IsSortedAscending(array1); // 1 - 5
+            bool isSortedDescending = IsSortedDescending(array1); // 5 - 1
+            if (isSortedAscending)
+            {
+                Console.WriteLine("The array is ordered by growth.");
+            }
+            else if (isSortedDescending)
+            {
+                Console.WriteLine("The array is ordered in descending order.");
+            }
+            else
+            {
+                Console.WriteLine("The array is not ordered.");
+            }
+
+            Console.ReadKey();
+
+        }
+        static bool IsSortedDescending(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > array[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private static bool IsSortedAscending(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < array[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    
+
+        private static void ArrayFiller(int[] array)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomElem = rnd.Next(0, 5);
+                array[i] = randomElem;
+            }
+        }
+    }
+}
---
+++ HillelCourse/HomeWork5/RemoveElemInArray/Program.cs
@@ -0,0 +1,60 @@
+﻿//Написати програму, в якій з масиву видаляється один елемент з позиції, яку вводить користувач. 
+
+
+namespace RemoveElemInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] array = new int[random.Next(2, 10)];
+            ArrayFiller(array);
+            foreach (int i in array)
+            {
+                Console.Write($"{i} ");
+            }
+            
+            int indexToRemove = GetTheElemToDelete(array);
+            int[] newArray = ElementRemover(indexToRemove, array);
+            Console.WriteLine("New Array is: ");
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.Write($"{newArray[i]} ") ;
+            }
+            Console.ReadKey();
+        }
+
+        private static int GetTheElemToDelete(int[] array)
+        {
+            Console.WriteLine($"\nWhat elem do you want to remove? 1\\{array.Length}");
+            int indexToRemove = int.Parse(Console.ReadLine());
+            return indexToRemove;
+        }
+
+        private static int[] ElementRemover(int remover, int[] array)
+        {
+            int[] newArray = new int[array.Length - 1];
+            for (int i = 0, j = 0; i < array.Length; i++)
+            {
+                if (i != remover - 1)
+                {
+                    newArray[j] = array[i];
+                    j++;
+                }
+            }
+            return newArray;
+        }
+
+        private static void ArrayFiller(int[] array)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomElem = rnd.Next(0, 100);
+                array[i] = randomElem;
+            }
+        }
+    }
+}
