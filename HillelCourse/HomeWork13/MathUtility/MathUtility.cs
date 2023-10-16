6cc2d83ba357b3de250f6779850c8dc4508483c2
Olejjas <mollstam4455@gmail.com>
2023-10-16 16:09:39 +0300

    HomeWork13 static class done 2/2

---
+++ HillelCourse/HomeWork13/MathUtility/MathUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace MathUtility
+{
+    public static class MathUtility
+    {
+        public static string Addition(int firstNum, int secondNum) => $"{firstNum} + {secondNum} = {firstNum + secondNum}";
+        public static string Subtraction(int firstNum, int secondNum) => $"{firstNum} - {secondNum} = {firstNum - secondNum}";
+
+        public static string Multiplication(int firstNum, int secondNum) => $"{firstNum} * {secondNum} = {firstNum * secondNum}";
+        public static string Division(int firstNum, int secondNum)
+        {
+            try { return $"{firstNum} / {secondNum} = {firstNum / secondNum}"; }
+            catch (DivideByZeroException)
+            {
+                return $"Error : Division of {secondNum} by zero.";
+            }
+        }
+
+    }
+}
---
+++ HillelCourse/HomeWork13/MathUtility/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MathUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 21;
+            int b = 3;
+            Console.WriteLine(MathUtility.Addition(a, b));
+            Console.WriteLine(MathUtility.Subtraction(a, b));
+            Console.WriteLine(MathUtility.Multiplication(a, b));
+            Console.WriteLine(MathUtility.Division(a, b));
+            Console.WriteLine(MathUtility.Division(a, 0));
+            
+            Console.ReadKey();
+        }
+    }
+}
